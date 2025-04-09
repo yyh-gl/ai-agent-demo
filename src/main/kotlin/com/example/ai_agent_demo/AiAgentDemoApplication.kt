7071d7491dcfaaaa980baf3e34847d58f9b707e6
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-09 21:48:34 +0900

    first commit

---
+++ src/main/kotlin/com/example/ai_agent_demo/AiAgentDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ai_agent_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiAgentDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiAgentDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ai_agent_demo/AiAgentDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ai_agent_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiAgentDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
